23a2538a5e61597c7dc1a6ea0147bce1423c88b2
John Sadie <johnsadie19@gmail.com>
2016-09-14 00:48:52 -0700

    creating struct for our data types

--- CS430Pro1/main.c
+++ CS430Pro1/main.c
@@ -1,6 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int width;
+int height;
+
+//defining structure variables
+typedef struct RGBPixel{
+     unsigned char r,g,b;
+} RGBPixel;
+//defining structure variables
+typedef struct PPMFormat{
+     int x, y;
+     RGBPixel *data;
+} PPMFormat;
+
+
 
 int main(int argc, char *argv[]){
     printf("Did it work?\n");
